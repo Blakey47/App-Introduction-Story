a37192351d11c0b635d07a0fea16dc92126b3462
Darragh <darragh.blake@urbanairship.com>
2020-01-15 13:42:29 +0000

    Configured UICollectionView Cells

--- Simple-Animated-Intro-Screens/ViewController.swift
+++ Simple-Animated-Intro-Screens/ViewController.swift
@@ -12,26 +12,46 @@ import Lottie
 class ViewController: UIViewController {
     
     let firstSlideLabel = SlideLabelButton(text: "Welcome")
-    let animatedView = AnimationView(name: "welcome")
+    let animatedView = AnimationView(name: "thinking")
     var padding: CGFloat = 20
     
-    let collectionView: UICollectionView = {
+    lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 0
         let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        cv.backgroundColor = .systemRed
+        cv.backgroundColor = .red
+        cv.dataSource = self
+        cv.delegate = self
+        cv.isPagingEnabled = true
         return cv
     }()
+    
+    let cellId = "cellId"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         configure()
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
     }
 
     func configure() {
+        configureCollectionView()
+    }
+    
+    func configureCollectionView() {
         view.addSubview(collectionView)
-//        configureFirstSlideLabel()
-//        configureFirstAnimatedView()
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+
+        
     }
     
 //    func configureFirstSlideLabel() {
@@ -60,5 +80,24 @@ class ViewController: UIViewController {
 //        ])
 //    }
 
+}
+
+
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = .white
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: view.frame.height)
+    }
+    
 }
 
